f444c1bce481d154119617cf43a3469989858a44
Захаров Антон <avti1308@yandex.ru>
2014-02-20 03:33:32 -0800

    Причесал TypoTest.php
    Добавил дополнительные проверки в тесты testSetUnknownCharset() и testSetUnknownEncoding().

--- typo/library/Typo/tests/TypoTest.php
+++ typo/library/Typo/tests/TypoTest.php
@@ -1,15 +1,113 @@
 <?php
-$root    = realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
-$include = "{$root}/../../include";
+
+$root    = realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
+$include = "{$root}/include";
 set_include_path(get_include_path() . PATH_SEPARATOR . $include);
 
-require_once "{$root}/../../Typo.php";
+require_once "{$root}/Typo/library/Typo.php";
+
 use Typo\Exception;
 
-class TypoTest extends PHPUnit_Framework_TestCase {
+class TypoTest extends PHPUnit_Framework_TestCase 
+{
+    /**
+     * Типограф.
+     * 
+     * @var \Typo
+     */
     protected $typo;
 
-    public function charsets() {
+    protected function setUp()
+    {
+        $this->typo = new Typo();
+    }
+
+    /**
+     * Проверяем значения свойств по умолчанию.
+     */
+    public function testDefaultOptions()
+    {
+        $this->assertEquals($this->typo->getOption('charset'), 'utf-8',  '', 0, 10, false, true);
+        $this->assertEquals($this->typo->getOption('encoding'), Typo::MODE_NAMES);
+        $this->assertEquals($this->typo->getOption('html-in-enabled'), true);
+        $this->assertEquals($this->typo->getOption('html-out-enabled'), true);
+        $this->assertEquals($this->typo->getOption('html-doctype'), Typo::DOCTYPE_HTML5);
+        $this->assertEquals($this->typo->getOption('nl2br'), true);
+        $this->assertEquals($this->typo->getOption('e-convert'), false);
+    }
+
+    /**
+     * Установка неизвестного параметра.
+     */
+    public function testSetUnknownOption() 
+    {
+        $this->setExpectedException('Typo\Exception', 'Несуществующий параметр');
+        $this->typo->setOption('unknown', 'value');
+    }
+    
+    /**
+     * Получение неизвестного параметра.
+     */
+    public function testGetUnknownOption() 
+    {
+        $this->setExpectedException('Typo\Exception', 'Несуществующий параметр');
+        $this->typo->getOption('unknown');
+    }
+    
+    
+    /**
+     * Установка неправильной кодировки.
+     */
+    public function testSetUnknownCharset() 
+    {
+        $charset = $this->typo->getOption('charset');
+        
+        $this->setExpectedException('Typo\Exception', 'Неизвестная кодировка', Exception::E_OPTION_VALUE);
+        $this->typo->setOption('charset', 'unknown');
+        
+        $this->assertEquals($this->typo->getOption('charset'), $charset);
+    }
+
+    /**
+     * Установка существуеющей кодировки.
+     * 
+     * @dataProvider charsets
+     */
+    public function testSetExistingCharset($charset) 
+    {
+        $this->typo->setOption('charset', $charset);
+        $this->assertEquals($this->typo->getOption('charset'), $charset, '', 0, 10, false, true);
+    }
+
+    /**
+     * Установка неправильного параметра кодировки.
+     */
+    public function testSetUnknownEncoding() 
+    {
+        $encoding = $this->typo->getOption('encoding');
+        
+        $this->setExpectedException('Typo\Exception', 'Неизвестный режим кодирования спецсимволов', Exception::E_OPTION_VALUE);
+        $this->typo->setOption('encoding', 'MODE_UNKNOWN');
+        
+        $this->assertEquals($this->typo->getOption('encoding'), $encoding);
+    }
+
+    /**
+     * Установка существующего параметра кодировки.
+     * 
+     * @dataProvider encodings
+     */
+    public function testSetExistingEncodings($encoding) 
+    {
+        $this->typo->setOption('encoding', $encoding);
+        $this->assertEquals($this->typo->getOption('encoding'), $encoding, '', 0, 10, false, true);
+    }
+    
+    /**
+     * Кодировки.
+     */
+    public function charsets() 
+    {
         return array(
             array('UTF-8'),
             array('CP1251'),
@@ -19,69 +117,17 @@ class TypoTest extends PHPUnit_Framework_TestCase {
             array('MAC')
         );
     }
-    public function encodings() {
+    
+    /**
+     * Режими кодирования спецсимволов.
+     */
+    public function encodings() 
+    {
         return array(
-            array('MODE_NONE'),
-            array('MODE_NAMES'),
-            array('MODE_CODES'),
-            array('MODE_HEX_CODES')
+            array(Typo::MODE_NONE),
+            array(Typo::MODE_NAMES),
+            array(Typo::MODE_CODES),
+            array(Typo::MODE_HEX_CODES)
         );
-    }
-
-    protected function setUp()
-    {
-        $this->typo = new Typo();
-    }
-
-    // Проверяем значения свойств по умолчанию
-    public function testDefaultOptions()
-    {
-        $this->assertEquals($this->typo->getOption('charset'), 'utf-8',  '', 0, 10, false, true);
-        $this->assertEquals($this->typo->getOption('encoding'), Typo::MODE_NAMES);
-
-        $modules = array('Html', 'Punct', 'Space', 'Dash', 'Nobr', 'Url', 'Quote', 'Math', 'Filepath', 'Smile\Kolobok\Standart');
-        array_walk($modules,function(&$item) { $item = 'Typo\Module\\' . $item; });
-        $this->assertEquals($this->typo->getOption('modules'), $modules);
-
-        $this->assertEquals($this->typo->getOption('html-in-enabled'), true);
-        $this->assertEquals($this->typo->getOption('html-out-enabled'), true);
-        $this->assertEquals($this->typo->getOption('html-doctype'), Typo::DOCTYPE_HTML5);
-        $this->assertEquals($this->typo->getOption('nl2br'), true);
-        $this->assertEquals($this->typo->getOption('e-convert'), false);
-    }
-
-    // Установка неизвестного параметра
-    public function testSetUnknownParam() {
-        $this->setExpectedException('Exception','Несуществующий параметр');
-        $this->typo->setOption('unknown', 'value');
-    }
-    // Установка неправильной кодировки
-    public function testSetUnknownCharset() {
-        $this->setExpectedException('Exception',"Неизвестная кодировка",Exception::E_OPTION_VALUE);
-        $this->typo->setOption('charset', 'UNKNOWN');
-    }
-
-    // Установка существуеющей кодировки
-    /**
-     * @dataProvider charsets
-     */
-    public function testSetExistingCharset($charset) {
-        $this->typo->setOption('charset', $charset);
-        $this->assertEquals($this->typo->getOption('charset'), $charset,'', 0, 10, false, true);
-    }
-
-    // Установка неправильного параметра кодировки
-    public function testSetUnknownEncoding() {
-        $this->setExpectedException('Exception','Неизвестный режим кодирования спецсимволов',Exception::E_OPTION_VALUE);
-        $this->typo->setOption('encoding', 'MODE_UNKNOWN');
-    }
-
-    // Установка существующего параметра кодировки
-    /**
-     * @dataProvider encodings
-     */
-    public function testSetExistingEncodings($encoding) {
-        $this->typo->setOption('encoding', $encoding);
-        $this->assertEquals($this->typo->getOption('encoding'), $encoding,'', 0, 10, false, true);
     }
 }
