039e5cc2212c54e9fd7accda733ee14e2573d916
Захаров Антон <avti1308@yandex.ru>
2014-02-20 15:45:52 +0400

    Перенёс TypoTest.php

--- Typo/tests/TypoTest.php
+++ Typo/tests/TypoTest.php
@@ -1,6 +1,6 @@
 <?php
 
-$root    = realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
+$root    = realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
 $include = "{$root}/include";
 set_include_path(get_include_path() . PATH_SEPARATOR . $include);
 
